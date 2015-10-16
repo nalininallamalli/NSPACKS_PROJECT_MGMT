b63fa026e1a05d859b52d9af1211156f366fc3c4
Amruta Kulkarni <amruta.rk2@gmail.com>
2015-10-16 16:02:13 +0530

    Adding AnalyticsViewModel

---
+++ HackathonPMA/HackathonPMA/Models/AnalyticsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace HackathonPMA.Models
+{
+    public class ByProjectViewModel
+    {
+        public int ProjectId { get; set; }
+
+        public string ProjectName { get; set; }
+
+        [Display(Name = "Projects")]
+        public SelectList ProjectList { get; set; }
+
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+    }
+
+    public class ByFundsViewModel
+    {
+        [Display(Name = "Project Id")]
+        public int ProjectId { get; set; }
+
+        public double Fund { get; set; }
+    }
+
+    public class CustomViewModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        public double Fund { get; set; }
+
+    }
+}
+
+
+
+
+
+
+
