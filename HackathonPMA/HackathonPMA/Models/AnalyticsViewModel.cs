41d72f7a42aca7cbc6e24d8cfd668fd2fc59f239
Amruta Kulkarni <amruta.rk2@gmail.com>
2015-10-29 05:46:28 +0530

    Project to Fund graph from database

--- HackathonPMA/HackathonPMA/Models/AnalyticsViewModel.cs
+++ HackathonPMA/HackathonPMA/Models/AnalyticsViewModel.cs
@@ -11,13 +11,19 @@ namespace HackathonPMA.Models
     {
         public int ProjectId { get; set; }
 
+        [Required]
+        [Display(Name = "Project Name: ")]
         public string ProjectName { get; set; }
 
-        [Display(Name = "Projects")]
-        public SelectList ProjectList { get; set; }
+        public IEnumerable<System.Web.Mvc.SelectListItem> ProjectList { get; set; }
 
-        [Display(Name = "Location")]
+        [Display(Name = "Location: ")]
         public string Location { get; set; }
+
+        public IEnumerable<System.Web.Mvc.SelectListItem> LocationList { get; set; }
+
+        public int[] instancesList { get; set; }
+
     }
 
     public class ByFundsViewModel
@@ -26,6 +32,10 @@ namespace HackathonPMA.Models
         public int ProjectId { get; set; }
 
         public double Fund { get; set; }
+
+        public int[] Logins { get; set; }
+
+
     }
 
     public class CustomViewModel
